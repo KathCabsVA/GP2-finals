dcec764a2dca855bd0afa1bfd7b2fc482ac7c765
KathCabsVA <155535379+KathCabsVA@users.noreply.github.com>
2024-03-19 12:18:02 +0800

    aded the folders

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarterAssets;
+
+public class Gun : MonoBehaviour
+{
+    private StarterAssetsInputs _input;
+    [SerializeField]
+    private GameObject bulletPrefab;
+    [SerializeField]
+    private GameObject bulletPoint;
+    [SerializeField]
+    private float bulletSpeed = 600;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _input = transform.root.GetComponent<StarterAssetsInputs>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Debug.Log("shoot!");
+        GameObject bullet = Instantiate(bulletPrefab, bulletPoint.transform.position, transform.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
+        Destroy(bullet, 1);
+    }
+}
