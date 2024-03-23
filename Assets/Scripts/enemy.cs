7a0e80ea87c271aeff16311619c37e75ec03a618
Patrick Ramos <patrick.ramos@ciit.edu.ph>
2024-03-23 12:15:54 +0800

    Merge branch 'pat-branch' into Kath

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -1,30 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemy : MonoBehaviour
 {
+
+    public NavMeshAgent agent;
+    public Transform player;
 
     public float moveSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyMove();
     }
 
     void enemyMove()
     {
-        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+        //GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
 
-        Transform player = playerObject.transform;
+        //Transform newplayer = playerObject.transform;
 
-        transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
-        Debug.DrawLine(transform.position, player.position, Color.red);
+        //transform.position = Vector3.MoveTowards(transform.position, newplayer.position, moveSpeed * Time.deltaTime);
+        //Debug.DrawLine(transform.position, newplayer.position, Color.red);
+        agent.SetDestination(player.position);
+    }
+    //pag nag collide sa player destroy enemy object
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
